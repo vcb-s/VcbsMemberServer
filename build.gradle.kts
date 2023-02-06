fd22641157a82c0ae343a701351bf2bb1c0f23ff
inori_lover <inori_lover@outlook.com>
2023-02-06 23:45:50 +0800

    feat: 稳定web security对授权的要求，接入 Argon2 实现

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.0.2"
-	id("io.spring.dependency-management") version "1.1.0"
+    java
+    id("org.springframework.boot") version "3.0.2"
+    id("io.spring.dependency-management") version "1.1.0"
 }
 
 group = "org.vcb-s"
@@ -9,24 +9,28 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	maven { url = uri("https://maven.aliyun.com/repository/public/") }
-	maven { url = uri("https://maven.aliyun.com/repository/google/") }
-	maven { url = uri("https://maven.aliyun.com/repository/spring/") }
-	maven { url = uri("https://maven.aliyun.com/repository/spring-plugin/") }
-	maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
-	mavenLocal()
-	mavenCentral()
+    maven { url = uri("https://maven.aliyun.com/repository/public/") }
+    maven { url = uri("https://maven.aliyun.com/repository/google/") }
+    maven { url = uri("https://maven.aliyun.com/repository/spring/") }
+    maven { url = uri("https://maven.aliyun.com/repository/spring-plugin/") }
+    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
+    mavenLocal()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.session:spring-session-core")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.session:spring-session-core")
     implementation("org.jetbrains:annotations:23.0.0")
+    // 提供 Argon2 相关支持
+    // https://mvnrepository.com/artifact/org.bouncycastle/bcpkix-jdk15on
+    implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- src/main/java/org/vcbs/member/configurations/WebSecurityConfig.java
+++ src/main/java/org/vcbs/member/configurations/WebSecurityConfig.java
@@ -1,20 +1,44 @@
-//package org.vcbs.member.configurations;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class WebSecurityConfig {
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
-//
-//        return manager;
-//    }
-//}
+package org.vcbs.member.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    PasswordEncoder passwordEncoder = createDelegatingPasswordEncoder();
+
+    /**
+     * spring boot推荐业务自己实现一个 PasswordEncoderFactories.createDelegatingPasswordEncoder
+     * 而不是直接使用项目的default；主要说明时考虑到最佳实践随时变更，但作为框架不能；
+     *
+     * @return {@link PasswordEncoder}
+     */
+    private static PasswordEncoder createDelegatingPasswordEncoder() {
+        String encodingId = "argon2@SpringSecurity_v5_8";
+
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put(encodingId, Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+
+        return new DelegatingPasswordEncoder(encodingId, encoders);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user").password(passwordEncoder.encode("password")).roles("USER").build());
+        
+        return manager;
+    }
+}
